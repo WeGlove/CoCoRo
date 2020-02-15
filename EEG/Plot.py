1fe2601a48bb58fde54e174bc5483f57b8522aea
Thorsten Schamper <thorsten.schamper@gmail.com>
2020-02-15 13:55:21 +0100

    implemented GUI thread which starts and updates the EEG signal plots. at the moment the numpy array containing the eeg data matrix has to be a global variable. Also the threads get not closed correctly.

--- EEG/Plot.py
+++ EEG/Plot.py
@@ -1,9 +1,12 @@
+import time
+
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 
 import random
 import colorsys
+import threading
 
 
 def generate_color():
@@ -14,6 +17,8 @@ def generate_color():
 class GUI:
 
     def __init__(self):
+
+        # self.data = data
 
         self.offset = 0  # just for debugging to simulate the EEG data flow.
 
@@ -28,26 +33,53 @@ class GUI:
         # add one curve to each subplot
         self.curves = [plot.plot() for plot in self.plots]
 
-        self.data = np.random.rand(8, 25000)
-        self.data -= .5
-        self.data *= 2
-
         # until proper color are chosen, just using random ones.
         self.colors = [generate_color() for _ in range(8)]
 
     def plot(self):
+        global data
         for index, curve in enumerate(self.curves):
             pen = pg.mkPen(self.colors[index], style=QtCore.Qt.SolidLine)
-            curve.setData(self.data[index][self.offset:self.offset + 1250], pen=pen)
+            # curve.setData(self.data[index][self.offset:self.offset + 1250], pen=pen)
+            curve.setData(data[index][-1250:], pen=pen)
 
     def update(self):
         self.offset += 1
         self.plot()
 
 
-gui = GUI()
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
 
-timer = pg.QtCore.QTimer()
-timer.timeout.connect(gui.update)
-timer.start(1)
-QtGui.QApplication.instance().exec_()
+    return wrapper
+
+
+class GUI_thread:
+    @threaded
+    def run(self):
+        _gui = GUI()
+        timer = pg.QtCore.QTimer()
+        timer.timeout.connect(_gui.update)
+        timer.start(4)
+        QtGui.QApplication.instance().exec_()
+
+
+data = np.random.rand(8, 2500)
+data -= .5
+data *= 2
+
+gui = GUI_thread()
+handle = gui.run()
+
+while True:
+    new_data = np.random.rand(8, 1)
+    new_data -= .5
+    new_data *= 2
+    data = np.c_[data, new_data]
+    time.sleep(.004)  # 250 hz
+    # print(data[0][-1:])
+
+handle.join()
